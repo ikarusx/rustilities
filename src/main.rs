cfb76613c897834b91630e20c4480f3c4b4b20d2
ikarusx <limbar@hotmail.com>
2019-06-26 18:02:54 -0700

    Added a string to type samples and documentation sample to main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+//! # Main
+//! 
+//! This is the entry point of the application,
+//! and this is an example for file documentation.
+//! It will document the file it is **in**, using *markdown*.
+
+/*! # Obs
+ * This is another way to add multiline documentation/lists.
+ * 1
+ * 2
+ * 3
+ */
+
 mod samples;
 mod tests;
 
@@ -12,6 +25,24 @@ fn test04_lambdas()
 {
     
 }
+
+/// # fn main
+/// 
+/// Main entry-point function.
+/// This is an example of documentation
+/// to items following it.
+/// 
+/// You can open/test it with the command:
+/// `cargo doc --open`
+/// 
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
 
 fn main()
 {
--- src/samples/s01_types.rs
+++ src/samples/s01_types.rs
@@ -1,6 +1,7 @@
 
 struct MyStruct
 {
+    my_name: String,
     my_int: i32,
     my_uint: u32,
     my_char: char,
@@ -11,12 +12,14 @@ impl MyStruct
     pub fn initialize() -> Self
     {
         println!("Initializing MyStruct ...");
+        let my_name = String::from("MyStruct");
         let my_int = -333;
         let my_uint = 333;
         let my_char = '†';
 
         Self
         {
+            my_name,
             my_int,
             my_uint,
             my_char
@@ -24,6 +27,7 @@ impl MyStruct
     }
 
     pub fn initialize_with_values(
+        my_name : String,
         my_int : i32,
         my_uint : u32,
         my_char : char) -> Self
@@ -32,6 +36,7 @@ impl MyStruct
 
         Self
         {
+            my_name,
             my_int,
             my_uint,
             my_char
@@ -40,7 +45,7 @@ impl MyStruct
 
     fn print_members(&self)
     {
-        println!("\nMembers are:");
+        println!("\nMembers of struct {} are:", self.my_name);
         println!("\tmy_int: {}", self.my_int);
         println!("\tmy_uint: {}", self.my_uint);
         println!("\tmy_char: {}\n", self.my_char);
@@ -54,6 +59,6 @@ pub fn sample01_structs_and_types()
     let my_struct = MyStruct::initialize();
     my_struct.print_members();
     let my_struct2 = MyStruct::initialize_with_values(
-        -1024, 1024, 'æ');
+        String::from("NamedStruct"), -1024, 1024, 'æ');
     my_struct2.print_members();
 }
